7ce714015ab77033347f25755ccbdf66981645da
Michael <mwu2015@mit.edu>
2013-05-06 19:03:04 -0400

    generator

--- src/network/GraphGenerator.java
+++ src/network/GraphGenerator.java
@@ -2,4 +2,23 @@ package network;
 
 public class GraphGenerator {
 
+	
+	
+	public static Graph generateCloseConnectGraph(int n, double threshold, double[][] range) {
+		
+	}
+	
+	public static Graph generateCloseProbGraph(int n, double[][] range) {
+		
+	}
+	
+	public static Graph generatePrefGraph(int n, double[][] range) {
+		
+	}
+
+	public static makeRandomPoints(int n, double[][] range) {
+		
+	}
+	
+	
 }
