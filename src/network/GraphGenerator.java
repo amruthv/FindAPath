6f5d380ad00bfd906bd5ccee76a3a3c287f070bb
Michael <mwu2015@mit.edu>
2013-05-06 19:07:54 -0400

    randompoints

--- src/network/GraphGenerator.java
+++ src/network/GraphGenerator.java
@@ -1,8 +1,11 @@
 package network;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphGenerator {
 
-	
+	// range is [[xLow, xHigh], [yLow, yHigh]]
 	
 	public static Graph generateCloseConnectGraph(int n, double threshold, double[][] range) {
 		
@@ -16,9 +19,15 @@ public class GraphGenerator {
 		
 	}
 
-	public static makeRandomPoints(int n, double[][] range) {
-		
+	public static List<Node> makeRandomPoints(int n, double[][] range) {
+		List<Node> nodes = new ArrayList<Node>();
+		for (int i = 0; i < n; i++)
+			nodes.add(new Node(getRandomInRange(range[0][0], range[0][1]), getRandomInRange(range[1][0], range[1][1])));
+		return nodes;
 	}
 	
+	public static double getRandomInRange(double low, double high) {
+		return low + Math.random() * (high - low);
+	}
 	
 }
