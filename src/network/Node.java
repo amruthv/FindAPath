57b92add1362fb1a25e41f538913ea4418a0b19c
Amruth Venkatraman <amruthv@mit.edu>
2013-05-06 18:48:41 -0400

    random fields for node

--- src/network/Node.java
+++ src/network/Node.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 public class Node {
     ArrayList<Link> links;
+    double xpos;
+    double ypos;
     
-    public Node() {
+    public Node(double xsize, double ysize) {
         links = new ArrayList<Link>();
+        
     }
 }
