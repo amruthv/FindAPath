575e357d54b99c4550cf3845a239085ea23ef196
Michael <mwu2015@mit.edu>
2013-05-11 22:21:32 -0400

    merge

---
+++ src/network/PacketAssigner.java
@@ -0,0 +1,16 @@
+package network;
+
+import java.util.Random;
+
+public class PacketAssigner {
+
+	public static void assignPackets(Graph graph, int p) {
+		Random r = new Random();
+		for (int i = 0; i < p; i++) {
+			Node from = graph.nodes.get(r.nextInt(graph.numNodes));
+			Node to = graph.nodes.get(r.nextInt(graph.numNodes));
+			Packet newPacket = new Packet(from, to, i);
+			from.addPacket(newPacket);
+		}
+	}	
+}
